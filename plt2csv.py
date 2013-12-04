7c82aa6d0b094d13bc737baf440985ba84bb210e
Hilary <hilary.e.p@gmail.com>
2013-12-03 18:44:36 -0800

    Added -d option for parsing Plotnik header

--- plt2csv.py
+++ plt2csv.py
@@ -33,9 +33,9 @@
 # mode, which should work with any likely configuration.
 #
 # See "USAGE" below for more information
-# 
+#
 # 2013-12-2 update: added handling for FAVE's new multiple measurement
-# output. New columns for F1 and F2 at 20, 35, 50, 65, and 80%. Also 
+# output. New columns for F1 and F2 at 20, 35, 50, 65, and 80%. Also
 # updated subject name regex to accommodate non PH-series data. -HEP
 
 from __future__ import with_statement  # Python 2.5 compatibility
@@ -45,27 +45,28 @@ from re import match
 from csv import DictWriter
 from sys import argv, stdout
 from itertools import islice
+from getopt import getopt, GetoptError
 
 NAN = float('nan')
 USAGE = 'USAGE: {} < PLT > CSV'.format(__file__)
 DELIMITER = ','
 SUBJECT = r'^\w\w\w?\d?\d?-\w?\d?\d?-\d?\d?\w?'
 
-VCLASSES = {1: 'i', 2: 'e', 3: 'ae', 5: 'o', 6: 'uh', 7: 'u', 11: 'iy', 
-            12: 'iyF', 14: 'iyr', 21: 'ey', 22: 'eyF', 24: 'eyr', 
+VCLASSES = {1: 'i', 2: 'e', 3: 'ae', 5: 'o', 6: 'uh', 7: 'u', 11: 'iy',
+            12: 'iyF', 14: 'iyr', 21: 'ey', 22: 'eyF', 24: 'eyr',
             33: 'aeh', 39: 'aeBR', 41: 'ay', 42: 'aw', 43: 'ah', 44: 'ahr',
             47: 'ay0', 53: 'oh', 54: 'ohr', 61: 'oy', 62: 'ow', 63: 'owF',
             64: 'owr', 72: 'uw', 73: 'Tuw', 74: 'uwr', 82: 'iw', 94: '*hr'}
-MANNERS = {0: '<n.a.>', 1: 'stop', 2: 'affricate', 3: 'fricative', 
+MANNERS = {0: '<n.a.>', 1: 'stop', 2: 'affricate', 3: 'fricative',
            4: 'nasal', 5: 'lateral', 6: 'rhotic'}
 # changed: "central" (huh?) to "rhotic" --KBG
-PLACES = {0: '<n.a.>', 1: 'labial', 2: 'labiodental', 3: 'interdental', 
+PLACES = {0: '<n.a.>', 1: 'labial', 2: 'labiodental', 3: 'interdental',
           4: 'alveolar', 5: 'alveopalatal', 6: 'velar'}
 # changed: "apical" to "aveolar" (this isn't Belfast) and "palatal" to
 # "alveopalatal" (this isn't Beijing) --KBG
 VOICES = {0: '<n.a.>', 1: 'voiceless', 2: 'voiced'}
-PRE_SEGS = {0: '<n.a.>', 1: 'oral labial', 2: 'm', 3: 'oral alveolar', 
-            4: 'n', 5: 'alveopalatal', 6: 'velar', 7: 'liquid', 
+PRE_SEGS = {0: '<n.a.>', 1: 'oral labial', 2: 'm', 3: 'oral alveolar',
+            4: 'n', 5: 'alveopalatal', 6: 'velar', 7: 'liquid',
             8: 'obstruent-liquid', 9: 'glide'}
 # changed: "nasal labial" to "m" (it's the only one), "apical" to "oral
 # alveolar" (as [n] is not included), "nasal apical" to "n" (it's the only
@@ -84,13 +85,11 @@ def plt2csv(source, sink, subject):
     """
     # first line of header
     (s, a, x, e, c, n, y) = source.readline().rstrip().split(DELIMITER)
-    row = {'Subject': subject, 'Speaker': s.strip()}
 
-    # in case we start trusting .plt header version:
-    #row = {'Subject': subject, 'Speaker': s.strip(), 'Age': a, 'Sex': x, 
-    #       'Ethnicity': e, 'Schooling': c if c != '' else NAN,
-    #       'Neighborhood': n, 'Year': y}
-    
+    row = {'Subject': subject, 'Speaker': s.strip(), 'Age': a, 'Sex': x,
+          'Ethnicity': e, 'Schooling': c if c != '' else NAN,
+          'Neighborhood': n, 'Year': y}
+
     # tells us where "summary" begins
     n_rows = int(source.readline().split(',')[0])
     for (i, line) in enumerate(islice(source, 0, n_rows)):
@@ -106,10 +105,11 @@ def plt2csv(source, sink, subject):
             (w, vt) = wp.split('<', 1)
             row['Word'] = w.split(None)[0]
             (row['F1_20'], row['F2_20'], row['F1_35'], row['F2_35'],
-            row['F1_50'], row['F2_50'], row['F1_65'], row['F2_65'],
-            row['F1_80'], row['F2_80']) = vt.rstrip('>').split(',')
+             row['F1_50'], row['F2_50'], row['F1_65'], row['F2_65'],
+             row['F1_80'], row['F2_80']) = vt.rstrip('>').split(',')
         else:
-            row['Word'] = wp.split(None)[0] # no extra measurements in old files
+            # no extra measurements in old files
+            row['Word'] = wp.split(None)[0]
         # bizarrely, stress and duration are coded in the same column
         (s, d) = sd.split('.', 2)
         row['Stress'] = s
@@ -128,27 +128,34 @@ def plt2csv(source, sink, subject):
 
 
 if __name__ == '__main__':
-    # turn STDOUT into a DictWriter
-    sink = DictWriter(stdout, fieldnames=['Subject', 'Speaker',
-                                          'F1', 'F2', 'F3', 'Word',
-                                          'Stress', 'Duration', 'VClass',
-                                          'Manner', 'Place', 'Voice',
-                                          'PreSeg', 'FolSeq', 'F1_20', 'F2_20', 
-                                          'F1_35', 'F2_35', 'F1_50', 'F2_50',
-                                          'F1_65', 'F2_65', 'F1_80', 'F2_80'])
+    demographics = False
+    try:
+        (opts, args) = getopt(argv[1:], 'd')
+        for (opt, val) in opts:
+            if opt == '-d':
+                demographics = True
+            else:
+                raise GetoptError
+    except GetoptError, err:
+        exit(err)
 
-    # in case we start trusting .plt header version:
-    #sink = DictWriter(stdout, fieldnames=['Subject', 'Speaker', 'Age',
-    #                                      'Sex', 'Ethnicity', 'Schooling',
-    #                                      'Neighborhood', 'Year',
-    #                                      'F1', 'F2', 'F3', 'Word',
-    #                                      'Stress', 'Duration', 'VClass',
-    #                                      'Manner', 'Place', 'Voice',
-    #                                      'PreSeg', 'FolSeq'])
-    
+    # turn STDOUT into a DictWriter
+    fields_IDs = ['Subject', 'Speaker']
+    fields_demographics = ['Age', 'Sex', 'Ethnicity', 'Schooling',
+                           'Neighborhood', 'Year'] if demographics else []
+    fields_measures = ['F1', 'F2', 'F3', 'Word',
+                       'Stress', 'Duration', 'VClass',
+                       'Manner', 'Place', 'Voice',
+                       'PreSeg', 'FolSeq', 'F1_20', 'F2_20',
+                       'F1_35', 'F2_35', 'F1_50', 'F2_50',
+                       'F1_65', 'F2_65', 'F1_80', 'F2_80']
+    sink = DictWriter(stdout, fieldnames=fields_IDs + fields_demographics + fields_measures)
     sink.writeheader()
     # run it
-    for fname in argv[1:]:
-        subject = match(SUBJECT, path.split(fname)[1]).group(0)
+    for fname in args:
+        m = match(SUBJECT, path.split(fname)[1])
+        if m == None:
+            exit('Malformed argument: "' + fname + '"')
+        subject = m.group(0)
         with open(fname, 'rU') as source:
             plt2csv(source, sink, subject)
